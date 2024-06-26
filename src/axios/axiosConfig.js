b1fa16547a006b39e603eb8f9cd0e4f25c6f0525
temesgen2024 <162815146+temesgen2024@users.noreply.github.com>
2024-06-25 22:57:12 -0400

    update

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api",
-  baseURL:"https://evangadi-forum-backend-zg0r.onrender.com/api"
+  baseURL: "https://evangadi-forum-backend-zg0r.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
